572bddaa45df4352187b4b91c7404893d0051926
Eric-Alain <ericalain1@hotmail.com>
2021-09-12 20:01:28 -0400

    Styles to technical section

--- src/templates/technical-page.js
+++ src/templates/technical-page.js
@@ -9,24 +9,26 @@ import Layout from '../components/Layout';
 export const TechnicalPageTemplate = ({ title, pageSections }) => {
 
   return (
-    <Container>
-      <h1>{title}</h1>
-      <Row>
-        {pageSections.section.map((section, i) => (
-          <Col xs='12' key={i}>
-            <section>
-              <h2>{section.subheading}</h2>
-              <Row>
-                <Col>
-                  {section.image ? <PreviewCompatibleImage imageInfo={section} /> : null}                  
-                  <MarkdownContent content={section.text} className='markdown-content' />
-                </Col>
-              </Row>
-            </section>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <main>
+      <Container>
+        <h1 className='display-3 fw-bold mb-2 border-bottom'>{title}</h1>
+        <Row>
+          {pageSections.section.map((section, i) => (
+            <Col xs='12' key={i}>
+              <section>
+                <h2>{section.subheading}</h2>
+                <Row>
+                  <Col>
+                    {section.image ? <PreviewCompatibleImage imageInfo={section} /> : null}
+                    <MarkdownContent content={section.text} className='markdown-content' />
+                  </Col>
+                </Row>
+              </section>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </main>
   );
 };
 
